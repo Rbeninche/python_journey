7f9e3a8d3bb2b8187c103dc5207afbe26a5e43c7
Reginald Beninche <rbeninche@gmail.com>
2020-10-12 00:14:34 -0400

    added student_registration_system app

---
+++ student_registration_system/app.py
@@ -0,0 +1,60 @@
+student_list = []
+
+def create_student():
+    name = input("Please enter the new student's name: ")
+    student_data = {
+    'name': name,
+    'marks': []
+    }
+
+    return student_data
+
+
+def add_mark(student, mark):
+    student['marks'].append(mark)
+
+
+def calculate_average_mark(student):
+    number = len(student['marks'])
+    if number == 0:
+        return 0
+    total = sum(student['marks'])
+
+    return total / number
+
+def print_student_detail(student):
+    print("{}, average mark: {}.".format(student['name'], calculate_average_mark(student)))
+
+
+def print_student_list(students):
+    if len(students) == 0:
+        print("No student registered yet")
+    for i, student in enumerate(students):
+        print("ID: {}".format(i))
+        print_student_detail(student)
+
+def menu():
+    selection = input("Enter 'p' to print the student list,"
+    "'s' to add a new student,'a' to add a mark to a student, or 'q' to quit ")
+
+    while selection != 'q':
+
+        if selection == 'p':
+            print_student_list(student_list)
+        elif selection == 's':
+            student_list.append(create_student())
+        elif selection == 'a':
+            if len(student_list) == 0:
+                print("No student registered yet")
+            else:
+
+                student_id = int(input("Enter the student ID to add a mark to: "))
+                student = student_list[student_id]
+                new_mark = int(input("Enter the new mark to be added: "))
+                add_mark(student, new_mark)
+
+        selection = input("Enter 'p' to print the student list,"
+        "'s' to add a new student,'a' to add a mark to a student, or 'q' to quit ")
+
+
+menu()
